ff9d0f08bcb73a0c707877a20df8d72882377b19
fabricio <120188885+Fabricio-developer@users.noreply.github.com>
2024-07-15 13:26:12 +0100

    Fix: remove toggle dark mode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -29,17 +29,6 @@ export class HeaderComponent implements OnInit {
     document.body.classList.toggle('dark', this.darkMode);
   }
 
-  toggleDarkMode() {
-    this.darkMode = !this.darkMode;
-    document.body.classList.toggle('dark', this.darkMode);
-    if (this.darkMode) {
-      Preferences.set({ key: 'darkModeActivated', value: 'true' });
-    } else {
-      // localStorage.setItem('darkModeActivated', 'false');
-      Preferences.set({ key: 'darkModeActivated', value: 'false' });
-    }
-  }
-
   ngOnInit() {
   }
   ionViewWillEnter() {
