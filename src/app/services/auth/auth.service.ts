312cc58a5f26cad560859c6edec1a7520d19520f
fabricio <120188885+Fabricio-developer@users.noreply.github.com>
2024-07-15 18:36:28 +0100

    Feat: add auth service

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from "firebase/compat/app";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from './interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  user: any;
+  error: any;
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  public currentUser: Observable<User | null> = this.currentUserSubject.asObservable();
+
+  constructor(public auth: AngularFireAuth) {
+    this.auth.authState.subscribe(user => {
+      if (user) {
+        this.currentUserSubject.next({
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL
+        });
+      } else {
+        this.currentUserSubject.next(null);
+      }
+    });
+  }
+
+  async emailSignin(emali: string, password: string) {
+    try {
+      const credential = await this.auth.signInWithEmailAndPassword(emali, password);
+      this.user = credential.user;
+    } catch (error) {
+      this.error = error;
+
+    }
+  }
+
+  async googleSignin() {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.auth.signInWithPopup(provider);
+
+      this.user = credential.user;
+
+      if (credential.user) {
+        let user = credential.user;
+        this.currentUserSubject.next({
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL
+        });
+      } else {
+        this.currentUserSubject.next(null);
+      }
+
+
+
+    } catch (error) {
+      this.error = error;
+    }
+  }
+
+  async signOut() {
+    await this.auth.signOut();
+    this.user = null;
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBww2kW3S5IHbmGYANW2KRVyVHmKsS9HhA",
+    authDomain: "be220-a97ba.firebaseapp.com",
+    projectId: "be220-a97ba",
+    storageBucket: "be220-a97ba.appspot.com",
+    messagingSenderId: "369407131599",
+    appId: "1:369407131599:web:4309be025aea2873afdcba",
+    measurementId: "G-BNEEWW9Z4N"
+  }
 };
